7899b471933f7b80f7f434c2ae729d5b02803ffa
giorgionetg <giorgionetg@gmail.com>
2023-11-01 11:46:05 +0100

    zustand integration navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,8 +1,20 @@
 'use client';
+import { useEffect } from 'react';
 import useNavbarStore from '@/components/Navbar/useNavbarStore';
 
 export default function Navbar() {
-  const { isOpen, isMobile } = useNavbarStore();
+  const { isOpen, isMobile, handleMobile } = useNavbarStore();
+
   const isOpenState = isOpen ? `It's open` : `It's closed`;
-  return <>Navbar {isOpenState}</>;
+  const isMobileState = isMobile ? `It's a Mobile` : `It's a Desktop`;
+
+  useEffect(() => {
+    handleMobile(window.navigator.userAgent);
+  }, [isMobile, handleMobile]);
+
+  return (
+    <>
+      Navbar {isOpenState}. And {isMobileState}
+    </>
+  );
 }
--- src/components/Navbar/useNavbarStore.ts
+++ src/components/Navbar/useNavbarStore.ts
@@ -1,17 +1,21 @@
-import { create } from 'zustand'
+import { create } from 'zustand';
 
 export interface NavState {
-    isOpen: boolean;
-    isMobile: undefined | boolean;
-    handleOpen: () => void;
-    handleMobile: () => void;
+  isOpen: boolean;
+  isMobile: undefined | boolean;
+  handleOpen: () => void;
+  handleMobile: (navigator: string) => void;
 }
 
 const useNavbarStore = create<NavState>((set, get) => ({
-    isOpen: false,
-    isMobile: undefined,
-    handleOpen: () => {},
-    handleMobile: () => {}
-}))
+  isOpen: false,
+  isMobile: undefined,
+  handleOpen: () => {},
+  handleMobile: (navigator) => {
+    set((state) => ({
+      isMobile: /Mobi/i.test(navigator),
+    }));
+  },
+}));
 
 export default useNavbarStore;
