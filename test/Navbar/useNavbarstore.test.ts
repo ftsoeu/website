84d38fa5dd2391fc7d73bbb1d1b34dee7617c543
giorgionetg <giorgionetg@gmail.com>
2023-11-04 09:16:09 +0100

    testing useNavbarStore

---
+++ test/Navbar/useNavbarstore.test.ts
@@ -0,0 +1,18 @@
+import { renderHook } from '@testing-library/react-hooks';
+import useNavbarStore from '@/components/Navbar/useNavbarStore';
+
+describe('Test useNavbarStore', () => {
+  test('It should have isOpen false', () => {
+    const { result } = renderHook(() => useNavbarStore());
+
+    console.log(result.current);
+    expect(result.current.isOpen).toBe(undefined);
+  });
+
+  test('It should have isMobile undefined', () => {
+    const { result } = renderHook(() => useNavbarStore());
+
+    console.log(result.current);
+    expect(result.current.isMobile).toBe(undefined);
+  });
+});
