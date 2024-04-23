cf8c04f7805ecc5b337962b575af6f0d1107ebb6
giorgionetg <giorgionetg@gmail.com>
2024-04-23 15:14:39 +0200

    cleaning up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,51 +1,9 @@
-//import Image from "next/image";
-
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from '@/components/Accordion/Accordion';
-import Navbar from '@/components/Navbar';
-import {
-  Pagination,
-  PaginationContent,
-  PaginationEllipsis,
-  PaginationItem,
-  PaginationLink,
-  PaginationNext,
-  PaginationPrevious,
-} from '@/components/ui/pagination';
+import Navbar from '@/components/Navbar/NavBar';
 
 export default function Home() {
   return (
     <>
-      White page + <Navbar />
-      <br />
-      <Accordion type='single' collapsible>
-        <AccordionItem value='item-1'>
-          <AccordionTrigger>Is it accessible?</AccordionTrigger>
-          <AccordionContent>
-            Yes. It adheres to the WAI-ARIA design pattern.
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion>
-      <Pagination>
-        <PaginationContent>
-          <PaginationItem>
-            <PaginationPrevious href='#' />
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationLink href='#'>1</PaginationLink>
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationEllipsis />
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationNext href='#' />
-          </PaginationItem>
-        </PaginationContent>
-      </Pagination>
+      <Navbar />
     </>
   );
 }
