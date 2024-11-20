87cd2daacc2b29efa0c57271f72c245e677139de
giorgionetg <giorgionetg@gmail.com>
2024-11-20 12:03:30 +0100

    add single page layout

--- src/app/single-page-layout/layout.tsx
+++ src/app/single-page-layout/layout.tsx
@@ -1,3 +1,4 @@
+import Breadcrumb from '@/components/Breadcrumb/Breadcrumb';
 import CallToAction from '@/components/CallToAction/CallToAction';
 import { ReactNode } from 'react';
 
@@ -8,11 +9,16 @@ interface ReadLayoutProps {
 export default function ReadLayout({ children }: ReadLayoutProps) {
   return (
     <>
-      <div className='bg-primary py-5'>
-        <div className='container text-[#fff]'>breadcrumb &gt; single page</div>
-      </div>
-      <div className='container mt-5'>
-        {children} <CallToAction action={'Delegate with us'} />
+      <Breadcrumb />
+      <div className='container mt-10'>
+        <div className='flex flex-col justify-between lg:flex-row'>
+          <article className='basis-2/3 prose prose-sm !max-w-none prose-headings:mt-0 prose-headings:mb-0 prose-p:mt-0 overflow-hidden w-full break-words whitespace-pre-wrap lg:prose lg:dark:prose-invert lg:prose-md text-wrap'>
+            {children}
+          </article>
+          <div className='basis-1/3'>
+            <CallToAction action={'Delegate with us'} />
+          </div>
+        </div>
       </div>
     </>
   );
