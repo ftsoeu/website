e5ea3dd38e2895bfe93dc6fa75d3835f241b9c57
giorgionetg <giorgionetg@gmail.com>
2025-01-14 07:51:28 +0100

    debug single article

--- src/app/news/[slug]/page.tsx
+++ src/app/news/[slug]/page.tsx
@@ -13,6 +13,10 @@ interface PageProps {
 }
 
 export async function generateStaticParams() {
+  console.log('Generating Single Article');
+  console.log('Called protected url: ', process.env.DIRECTUS_URL);
+  console.log('Called next public url: ', process.env.NEXT_PUBLIC_DIRECTUS_URL);
+
   const pages = await directus.request(
     readItems('Articles', {
       fields: ['*'],
